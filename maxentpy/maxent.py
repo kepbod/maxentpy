55968dc1053f58542617e0636f304457b2c9aeae
Xiao-Ou Zhang <kepbod@gmail.com>
2016-10-05 09:53:25 -0400

    preload matrix to speed up

--- maxentpy/maxent.py
+++ maxentpy/maxent.py
@@ -14,12 +14,20 @@ from string import maketrans
 import os.path
 
 
-__all__ = ['score5', 'score3']
+__all__ = ['score5', 'score3', 'load_matrix5', 'load_matrix3']
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
+bgd_5 = {'A': 0.27, 'C': 0.23, 'G': 0.23, 'T': 0.27}
+cons1_5 = {'A': 0.004, 'C': 0.0032, 'G': 0.9896, 'T': 0.0032}
+cons2_5 = {'A': 0.0034, 'C': 0.0039, 'G': 0.0042, 'T': 0.9884}
 
-def score5(fa):
+bgd_3 = {'A': 0.27, 'C': 0.23, 'G': 0.23, 'T': 0.27}
+cons1_3 = {'A': 0.9903, 'C': 0.0032, 'G': 0.0034, 'T': 0.0030}
+cons2_3 = {'A': 0.0027, 'C': 0.0037, 'G': 0.9905, 'T': 0.0030}
+
+
+def score5(fa, matrix=None):
     '''
     Calculate 5' splice site strength
     (exon)XXX|XXXXXX(intron)
@@ -30,27 +38,34 @@ def score5(fa):
     11.08
     >>> round(score5('taaATAAGT'), 2)
     -0.12
+    >>> matrix = load_matrix5()
+    >>> round(score5('cagGTAAGT', matrix=matrix), 2)
+    10.86
     '''
+    # check matrix
+    if not matrix:
+        matrix = load_matrix5()
     # for key elements
     key = fa[3:5].upper()
-    bgd = {'A': 0.27, 'C': 0.23, 'G': 0.23, 'T': 0.27}
-    cons1 = {'A': 0.004, 'C': 0.0032, 'G': 0.9896, 'T': 0.0032}
-    cons2 = {'A': 0.0034, 'C': 0.0039, 'G': 0.0042, 'T': 0.9884}
-    score = cons1[key[0]] * cons2[key[1]] / (bgd[key[0]] * bgd[key[1]])
+    score = cons1_5[key[0]] * cons2_5[key[1]] / (bgd_5[key[0]] * bgd_5[key[1]])
     # for rest elements
     rest = (fa[:3] + fa[5:]).upper()
-    matrix_f = dir_path + '/data/score5_matrix.txt'
-    with open(matrix_f, 'r') as f:
-        for line in f:
-            entry = line.split()
-            if entry[0] == rest:
-                rest_score = float(entry[1])
-                break
+    rest_score = matrix[rest]
     # final score
     return math.log(score * rest_score, 2)
 
 
-def score3(fa):
+def load_matrix5():
+    matrix_f = dir_path + '/data/score5_matrix.txt'
+    matrix = {}
+    with open(matrix_f, 'r') as f:
+        for line in f:
+            entry = line.split()
+            matrix[entry[0]] = float(entry[1])
+    return matrix
+
+
+def score3(fa, matrix=None):
     '''
     Calculate 3' splice site strength
     (intron)XXXXXXXXXXXXXXXXXXXX|XXX(exon)
@@ -61,21 +76,18 @@ def score3(fa):
     8.19
     >>> round(score3('ttctctcttcagacttatAGcaa'), 2)
     -0.08
+    >>> matrix = load_matrix3()
+    >>> round(score3('ttccaaacgaacttttgtAGgga', matrix=matrix), 2)
+    2.89
     '''
+    # check matrix
+    if not matrix:
+        matrix = load_matrix3()
     # for key elements
     key = fa[18:20].upper()
-    bgd = {'A': 0.27, 'C': 0.23, 'G': 0.23, 'T': 0.27}
-    cons1 = {'A': 0.9903, 'C': 0.0032, 'G': 0.0034, 'T': 0.0030}
-    cons2 = {'A': 0.0027, 'C': 0.0037, 'G': 0.9905, 'T': 0.0030}
-    score = cons1[key[0]] * cons2[key[1]] / (bgd[key[0]] * bgd[key[1]])
+    score = cons1_3[key[0]] * cons2_3[key[1]] / (bgd_3[key[0]] * bgd_3[key[1]])
     # for rest elements
     rest = (fa[:18] + fa[20:]).upper()
-    matrix_f = dir_path + '/data/score3_matrix.txt'
-    matrix = defaultdict(dict)
-    with open(matrix_f, 'r') as f:
-        for line in f:
-            n, m, s = line.split()
-            matrix[int(n)][int(m)] = float(s)
     rest_score = 1
     rest_score *= matrix[0][hashseq(rest[:7])]
     rest_score *= matrix[1][hashseq(rest[7:14])]
@@ -88,6 +100,16 @@ def score3(fa):
     rest_score /= matrix[8][hashseq(rest[14:18])]
     # final score
     return math.log(score * rest_score, 2)
+
+
+def load_matrix3():
+    matrix_f = dir_path + '/data/score3_matrix.txt'
+    matrix = defaultdict(dict)
+    with open(matrix_f, 'r') as f:
+        for line in f:
+            n, m, s = line.split()
+            matrix[int(n)][int(m)] = float(s)
+    return matrix
 
 
 def hashseq(fa):
