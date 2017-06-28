06473aefee7121ed67353caf9db5ed43464aa6e0
Xiao-Ou Zhang <kepbod@gmail.com>
2017-06-28 16:34:38 -0400

    add maxent_fast.py

--- maxentpy/__init__.py
+++
@@ -1,4 +0,0 @@
-try:
-    from maxent import score5, score3, load_matrix5, load_matrix3
-except ImportError:
-    from .maxent import score5, score3, load_matrix5, load_matrix3
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+
+ext_modules = [Extension('maxentpy._hashseq', ['maxentpy/_hashseq.pyx'])]
 
 setup(name='maxentpy',
       description='Calculate splice site strength',
       url='https://github.com/kepbod/maxentpy',
       packages=['maxentpy'],
-      package_data={'maxentpy': ['data/*.txt']}
+      package_data={'maxentpy': ['data/*']},
+      ext_modules=cythonize(ext_modules),
+      install_requires=['msgpack-python', 'Cython']
       )
