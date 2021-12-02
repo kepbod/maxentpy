ba62f49931b18e7598a05d31c1c9430bdf0006b1
Xiao-Ou Zhang <kepbod@gmail.com>
2021-12-02 09:27:39 +0800

    update to 0.02

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools.extension import Extension
 ext_modules = [Extension('maxentpy._hashseq', ['maxentpy/_hashseq.c'])]
 
 setup(name='maxentpy',
-      version='0.0.1',
+      version='0.0.2',
       description='Calculate splice site strength',
       url='https://github.com/kepbod/maxentpy',
       packages=['maxentpy'],
