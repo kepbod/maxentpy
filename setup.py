b5e69aadd745fcd9ee65c9dd0ff21b902a63a825
Xiao-Ou Zhang <kepbod@gmail.com>
2026-07-12 22:43:35 +0800

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from Cython.Build import cythonize
 ext_modules = cythonize([Extension('maxentpy._hashseq', ['maxentpy/_hashseq.pyx'])])
 
 setup(name='maxentpy',
-      version='0.0.3',
+      version='0.0.4',
       description='Calculate splice site strength',
       url='https://github.com/kepbod/maxentpy',
       packages=['maxentpy'],
